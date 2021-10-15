59fb1a9eae795fea19d97b4399380784c560d78e
dawei <dw15556046091@163.com>
2021-10-15 18:02:57 +0800

    修改加密数据

--- beck-service/beck-weixin/src/main/java/com/beck/weixin/utils/WXUtlis.java
+++ beck-service/beck-weixin/src/main/java/com/beck/weixin/utils/WXUtlis.java
@@ -3,19 +3,13 @@ package com.beck.weixin.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.beck.common.utils.StringUtils;
-import com.beck.common.utils.sign.Base64;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
 import java.security.*;
-import java.security.spec.InvalidParameterSpecException;
 import java.util.Arrays;
 
 /**
@@ -32,10 +26,6 @@ public class WXUtlis {
      */
     public static JSONObject dencryptedUserData(String encryptedData, String sessionKey, String iv) {
         JSONObject res = new JSONObject();
-        if(StringUtils.isBlank(encryptedData) || StringUtils.isBlank(sessionKey) || StringUtils.isBlank(iv)){
-            res.put("msg","加密数据、sessionKey、iv不能为空");
-            return res;
-        }
         // 被加密的数据
         byte[] dataByte = Base64.decode(encryptedData);
         // 加密秘钥
@@ -43,7 +33,6 @@ public class WXUtlis {
         // 偏移量
         byte[] ivByte = Base64.decode(iv);
         try {
-            // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
             int base = 16;
             if (keyByte.length % base != 0) {
                 int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
@@ -64,24 +53,8 @@ public class WXUtlis {
                 String result = new String(resultByte, "UTF-8");
                 return JSON.parseObject(result);
             }
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println(e.getMessage());
-        } catch (NoSuchPaddingException e) {
-            System.out.println(e.getMessage());
-        } catch (InvalidParameterSpecException e) {
-            System.out.println(e.getMessage());
-        } catch (IllegalBlockSizeException e) {
-            System.out.println(e.getMessage());
-        } catch (BadPaddingException e) {
-            System.out.println(e.getMessage());
-        } catch (UnsupportedEncodingException e) {
-            System.out.println(e.getMessage());
-        } catch (InvalidKeyException e) {
-            System.out.println(e.getMessage());
-        } catch (InvalidAlgorithmParameterException e) {
-            System.out.println(e.getMessage());
-        } catch (NoSuchProviderException e) {
-            System.out.println(e.getMessage());
+        } catch (Exception e){
+            e.printStackTrace();
         }
         return null;
     }
