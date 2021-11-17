9bb67026b94146f53be36bac9042ec69478b44a0
dawei <dw15556046091@163.com>
2021-11-17 16:20:59 +0800

    修改文件名

--- beck-app/beck-api/src/main/java/com/beck/ApiApplication.java
+++ beck-app/beck-api/src/main/java/com/beck/ApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * 启动程序
  */
-@SpringBootApplication(scanBasePackages = {"com.beck"} , exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ApiApplication
 {
     public static void main(String[] args)
--- beck-app/beck-wx/src/main/java/com/beck/WeiXinApplication.java
+++ beck-app/beck-wx/src/main/java/com/beck/WeiXinApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * 启动程序
  */
-@SpringBootApplication(scanBasePackages = {"com.beck"} , exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class WeiXinApplication
 {
     public static void main(String[] args)
